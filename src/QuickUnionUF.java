81e31bb69679f9bbac52f45aebca1ff2b2f22b16
VividTruthKeeper <mr.aydogdy02@gmail.com>
2022-12-07 19:01:44 +0500

    quick union

---
+++ src/QuickUnionUF.java
@@ -0,0 +1,32 @@
+public class QuickUnionUF {
+    private int[] id;
+
+    public QuickUnionUF(int N){
+        id = new int[N];
+        for(int i = 0; i < N; i++){
+            id[i] = i;
+        }
+    }
+
+    private int root(int i){
+        while(i != id[i]){
+            i = id[i];
+
+            //  [[2]] -> [2] -> 2
+        }
+        return i;
+    }
+
+    public boolean connected(int p, int q){
+        return root(p) == root(q);
+    }
+
+    public void union(int p, int q){
+        int i = root(p);
+        int j = root(q);
+        id[i] = j;
+
+        // O(N) = N
+    }
+    
+}
